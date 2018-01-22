8601c6e256d19a961779a4b636c4a3c37593a4e3
Serg4356 <asdfg111000@yandex.ru>
2018-01-22 14:49:43 +0500

    Write code. Add Readme.md

--- README.md
+++ README.md
@@ -1,6 +1,41 @@
 # Image Resizer
 
-[TODO. There will be project description]
+This command line script makes it easy to resize images. You can change width or height(or both) of your image, set the resized image scale, which will be saved to a custom folder(or default original image folder). If you break image proportion script will display a warning message.
+
+# How to install
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+```bash
+pip install -r requirements.txt # alternatively try pip3
+```
+
+# Quickstart
+
+Programm takes arguments: 
+```bash
+optional arguments:
+  -h, --help            show this help message and exit
+  -i INPUT, --input INPUT
+  -o OUTPUT, --output OUTPUT
+  -wh [WIDTH_HEIGHT [WIDTH_HEIGHT ...]], --width_height [WIDTH_HEIGHT [WIDTH_HEIGHT ...]]
+                        Width and Heigth of result image, only the first two
+                        arguments will be taken
+  -s SCALE, --scale SCALE
+                        Scale of resize
+```
+
+which you can explore symply by typing the following in command line:
+```bash
+$ python image_resize.py -h
+```
+
+Example of programm output:
+```bash
+$ python image_resize.py -i C:\devman\map.png -wh 1500 1200
+Result image proportion will be changed
+```
+After that you will find your resized image here: C:\devman\map__1500x1200.png
 
 # Project Goals
 
--- image_resize.py
+++ image_resize.py
@@ -1,45 +1,56 @@
-from __future__ import print_function
 import argparse
+import os
 from PIL import Image
-import re
 
 
 def create_parser():
     parser = argparse.ArgumentParser(description='Programm recizes images by input parameters')
     parser.add_argument('-i', '--input', required=True)
     parser.add_argument('-o', '--output')
-    group = parser.add_mutually_exclusive_group()
+    group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('-wh',
                        '--width_height',
                        type=int,
                        nargs='*',
-                       help='Width and Heigth of result image')
+                       help='Width and Heigth of result image, only the first two arguments will be taken')
     group.add_argument('-s', '--scale', type=float, help='Scale of resize')
     return parser
 
 
-def resize_image(path_to_original, path_to_result, scale, width, height):
+def resize_image(path_to_original, path_to_result, scale, width_height):
     original = Image.open(path_to_original)
     original_width = original.size[0]
     original_height = original.size[1]
     if scale:
         original.resize((original_width*scale, original_height*scale)).save(path_to_result)
     else:
-        if not (original_width/original_height) == width/height:
+        if not (original_width/original_height) == width_height[0]/width_height[1]:
             print('Result image proportion will be changed')
-        original.resize((width, height)).save(path_to_result)
+        original.resize((width_height[0],width_height[1])).save(path_to_result)
 
 
-def generate_image_name(path_to_original, path_to_result, scale, width, height):
-    if not path_to_result:
-        print(path_to_original)
-    print(re.findall(r'("[]")', path_to_original))
-
+def generate_image_name(path_to_original, path_to_result, scale, width_height):
+    (original_root, original_file_name) = os.path.split(path_to_original)
+    (original_prefix_name, original_extension) = os.path.splitext(original_file_name)
+    if scale:
+        result_name = '{}__x{}{}'.format(original_prefix_name,str(scale),original_extension)
+    else:
+        width = width_height[0]
+        height = width_height[1]
+        result_name = '{}__{}x{}{}'.format(original_prefix_name,str(width),str(height),original_extension)
+    if path_to_result:
+        if not os.path.splitext(path_to_result)[1]:
+            return os.path.join(path_to_result, result_name)
+        else:
+            return path_to_result
+    else:
+        return os.path.join(original_root, result_name)
 
 
 if __name__ == '__main__':
     parser = create_parser()
-    options = parser.parse_args() # ['-i G:\\devman\\778845102.jpg','-wh 2 3']
-    generate_image_name(options.input, options.output, options.scale, options.width_height[0], options.width_height[1])
-    # resize_image(parameters.input, parameters.output, parameters.scale, parameters.)
+    options = parser.parse_args()
+    path_to_result = generate_image_name(options.input, options.output, options.scale, options.width_height)
+    resize_image(options.input, path_to_result, options.scale, options.width_height)
+
 
